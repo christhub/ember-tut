3457c5ad021330e1a57b73e9d5f0c8b05e3c95a5
christian <christian.lei1@gmail.com>
2015-11-16 15:23:22 -0800

    adds a delete button

--- app/pods/post/index/route.js
+++ app/pods/post/index/route.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('post');
-  }
+  },
+  actions: {
+    deletePost(kitn) {
+      kitn.destroyRecord();
+    }
+  },
 });
